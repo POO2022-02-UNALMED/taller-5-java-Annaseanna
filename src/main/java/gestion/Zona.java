d04b94b68bca72dc76785f9a770267b4ca2be7d7
Anna Ospina Bedoya <85521007+Annaseanna@users.noreply.github.com>
2022-09-26 23:50:44 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,57 +1,42 @@
 package gestion;
-
 import java.util.ArrayList;
-
 import zooAnimales.Animal;
 
 public class Zona {
     private String nombre;
-    private Zoologico[] zoo = new Zoologico[1];
-    private ArrayList<Animal> animales = new ArrayList<Animal>();
+    private Zoologico zoo;
+    private ArrayList<Animal> animales = new ArrayList<>();
+
+    public Zona(String nombre, Zoologico zoo) {
+        this.nombre = nombre;
+        this.zoo = zoo;
+    }
 
     public Zona() {
         this(null, null);
     }
 
-    public Zona(String nombre, Zoologico zoo) {
-        this.nombre = nombre;
-        if (zoo != null) {
-            zoo.agregarZonas(this);
-        }
-    }
-
     public void agregarAnimales(Animal animal) {
-        this.animales.add(animal);
-        Zona[] x = { this };
-        animal.setZona(x);
+        animales.add(animal);
     }
 
     public int cantidadAnimales() {
-        return this.animales.size();
+        return animales.size();
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public Zoologico getZoo() {
-        return zoo[0];
-    }
-
-    public ArrayList<Animal> getAnimales() {
-        return animales;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    public Zoologico getZoo() {
+        return zoo;
+    }
+
     public void setZoo(Zoologico zoo) {
-        this.zoo[0] = zoo;
+        this.zoo = zoo;
     }
-
-    public void setAnimales(ArrayList<Animal> animales) {
-        this.animales = animales;
-    }
-
 }
